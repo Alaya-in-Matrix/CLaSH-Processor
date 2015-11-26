4a9b43af4130d235a5709530a8fc06b9fcb97dee
lvwenlong <lvwenlong_lambda@qq.com>
2015-11-26 20:22:36 +0800

    Seperate test code into Test.hs

--- Esprockell.hs
+++ Esprockell.hs
@@ -2,7 +2,6 @@
 module Esprockell where
 
 import CLaSH.Prelude hiding (Word)
-import qualified Data.List as L
 
 {-------------------------------------------------------------
 | Esprockell: Expanded Simple PROCessor in hasKELL 
@@ -222,39 +221,5 @@ sprockellMealy prog state inp = (PState {dmem = dmem', reg = reg', cnd = cnd', p
      pc'          = updatePC (jmpCode, cnd) (pc, jumpN, x)
      sp'          = updateSp spCode sp
      outp         = debug (dmem, reg) dbCode
-{------------------------------------- TEST ----------------------------------------------}
-r7  = 7  :: RegIdx
-r8  = 8  :: RegIdx
-r9  = 9  :: RegIdx
-r10 = 10 :: RegIdx
-r11 = 11 :: RegIdx
-r12 = 12 :: RegIdx
-r13 = 13 :: RegIdx
-r14 = 14 :: RegIdx
-r15 = 15 :: RegIdx
-r16 = 16 :: RegIdx
-
-prog1 :: [ISA]
-prog1 = [
-    Load (RImm 3) r7 
-  , Load (RImm 4) r8 
-  , Arith Id r7 r8 r9
-  , Debug (DebugReg r9)
-  , EndProg
-    ]
 
 sprockell imem = sprockellMealy imem `mealy` def
-
--- L.foldr :: Foldable t => (a -> b -> b) -> b -> t a -> b
--- replace :: (Enum i, KnownNat n) => i -> a -> Vec n a -> Vec n a
-createImem :: [ISA] -> IMem
-createImem prog = createImem' prog defaultImm
-  where createImem' p imm   = L.foldr pushIm imm $ L.zip [0..] p
-        pushIm (idx, v) imm = replace idx v imm
-        defaultImm = repeat EndProg :: IMem
-
-run :: [ISA] -> Signal (Maybe Word)
-run prog = let imm = createImem prog
-             in sprockell imm undefined
-
-samp sampNum prog = mapM_ print $ sampleN sampNum $ run prog
---
+++ Test.hs
@@ -0,0 +1,40 @@
+module Test where
+
+import CLaSH.Prelude hiding(Word)
+import Esprockell
+import qualified Data.List as L
+
+
+r7  = 7  :: RegIdx
+r8  = 8  :: RegIdx
+r9  = 9  :: RegIdx
+r10 = 10 :: RegIdx
+r11 = 11 :: RegIdx
+r12 = 12 :: RegIdx
+r13 = 13 :: RegIdx
+r14 = 14 :: RegIdx
+r15 = 15 :: RegIdx
+r16 = 16 :: RegIdx
+
+progAdd :: [ISA]
+progAdd = [
+    Load (RImm 3) r7 
+  , Load (RImm 4) r8 
+  , Arith Id r7 r8 r9
+  , Debug (DebugReg r9)
+  , EndProg
+    ]
+
+-- L.foldr :: Foldable t => (a -> b -> b) -> b -> t a -> b
+-- replace :: (Enum i, KnownNat n) => i -> a -> Vec n a -> Vec n a
+createImem :: [ISA] -> IMem
+createImem prog = createImem' prog defaultImm
+  where createImem' p imm   = L.foldr pushIm imm $ L.zip [0..] p
+        pushIm (idx, v) imm = replace idx v imm
+        defaultImm = repeat EndProg :: IMem
+
+run :: [ISA] -> Signal (Maybe Word)
+run prog = let imm = createImem prog
+             in sprockell imm undefined
+
+samp sampNum prog = mapM_ print $ sampleN sampNum $ run prog
